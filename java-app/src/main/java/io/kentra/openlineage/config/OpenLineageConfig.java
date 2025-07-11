cd1d462146e83def128742fff58b53f9ba6edd66
Jan Siekierski <jan.siekierski@kentra.io>
2025-07-11 13:44:57 +0200

    Full setup with docker compose

--- java-app/src/main/java/io/kentra/openlineage/config/OpenLineageConfig.java
+++ java-app/src/main/java/io/kentra/openlineage/config/OpenLineageConfig.java
@@ -1,33 +1,37 @@
 package io.kentra.openlineage.config;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import io.openlineage.client.Clients;
 import io.openlineage.client.OpenLineage;
 import io.openlineage.client.OpenLineageClient;
+import io.openlineage.client.OpenLineageClientUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 
 import java.net.URI;
-import java.nio.file.Paths;
-import java.util.List;
 
 @Configuration
 public class OpenLineageConfig {
-    public static final String OPEN_LINEAGE_PRODUCER_NAMESPACE = "https://kentra-openlineage-java-lib";
+  public static final String OPEN_LINEAGE_PRODUCER_NAMESPACE = "https://kentra-openlineage-java-lib";
 
-    @Value("${openlineage.config.location}")
-    public String openLineageConfigLocation;
+  @Value("${openlineage.config.location}")
+  public Resource openLineageConfigLocation;
 
-    @Bean
-    public OpenLineageClient openLineageClient() throws Exception {
-        URI openLineageConfigResource = new ClassPathResource(openLineageConfigLocation).getURI();
-        return Clients.newClient(() -> List.of(Paths.get(openLineageConfigResource)));
-    }
+  @Bean
+  public OpenLineageClient openLineageClient() throws Exception {
+    System.out.println("LOADING CLIENT");
+    var openLineageConfig = OpenLineageClientUtils.loadOpenLineageConfigYaml(
+        openLineageConfigLocation.getInputStream(),
+        new TypeReference<>() {}
+    );
+    return Clients.newClient(openLineageConfig);
+  }
 
-    @Bean
-    public OpenLineage openLineage() {
-        return new OpenLineage(URI.create(OPEN_LINEAGE_PRODUCER_NAMESPACE));
-    }
+  @Bean
+  public OpenLineage openLineage() {
+    return new OpenLineage(URI.create(OPEN_LINEAGE_PRODUCER_NAMESPACE));
+  }
 
 }
--- scripts/create-product.sh
+++ scripts/create-product.sh
@@ -1,3 +1,3 @@
-curl -X POST "http://localhost:8085/products" \
+curl -X POST "http://localhost:8087/products" \
   -H "Content-Type: application/json" \
   -d '{"id":null,"name":"Test Product","price":100,"description":null}'
